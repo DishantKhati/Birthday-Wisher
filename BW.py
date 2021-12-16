fde3edfd53b0f48830f21bf57c0da6f563ac3fab
KNIGHTUNITYDK <83055506+KNIGHTUNITYDK@users.noreply.github.com>
2021-12-16 21:56:25 +0530

    Add files via upload
    Birthday Wishing Program

---
+++ BW.py
@@ -0,0 +1,10 @@
+import datetime
+'''import pyttsx3
+engine=pyttsx3.init("sapi5")
+voices=engine.getProperty("voices")
+engine.setProperty("voices",voices[0].id)
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()'''
+today=datetime.datetime.now()
+print("Happy Birthday ",input("Enter the name: "),"\nDate: ",today.day," : ",today.month," : ",today.year)
